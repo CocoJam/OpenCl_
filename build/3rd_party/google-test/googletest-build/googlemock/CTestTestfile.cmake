ec7bf068fa695aa9f8ce6680454d435797460c18
CocoJam <ljam763@aucklanduni.ac.nz>
2019-01-30 17:01:32 +0800

    init

--- build/3rd_party/google-test/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/james/Desktop/cpp/leetcode/build/3rd_party/google-test/googletest-src/googlemock
-# Build directory: C:/Users/james/Desktop/cpp/leetcode/build/3rd_party/google-test/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
